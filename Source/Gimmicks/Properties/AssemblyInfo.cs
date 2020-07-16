c7eaa232db37e9c46d33cdc1ddac412bdac467cf
emipa606 <emil.offermo@litet.net>
2020-07-16 19:34:31 +0200

    First publish

---
+++ Source/Gimmicks/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Gimmicks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Accurate Gaming")]
+[assembly: AssemblyProduct("Gimmicks")]
+[assembly: AssemblyCopyright("Copyright © Trunken 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("97407369-cf7b-48d8-9d80-e4b477f1a225")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/Gimmicks/ThoughtWorker_WearingWhip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RimWorld;
+using Verse;
+
+namespace Gimmicks
+{
+	// Token: 0x02000003 RID: 3
+	public class ThoughtWorker_WearingWhip : ThoughtWorker
+	{
+		// Token: 0x06000002 RID: 2 RVA: 0x00002060 File Offset: 0x00000260
+		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			bool flag = p.equipment.Primary != null;
+			if (flag)
+			{
+				bool flag2 = p.equipment.Primary.def.defName == "Tool_Whip" || p.equipment.Primary.def.defName == "VWE_Tool_Whip";
+				bool flag3 = flag2;
+				if (flag3)
+				{
+					return ThoughtState.ActiveAtStage(0);
+				}
+			}
+			return ThoughtState.Inactive;
+		}
+	}
+}
