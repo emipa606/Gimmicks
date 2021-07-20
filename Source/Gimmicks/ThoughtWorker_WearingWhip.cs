d688cbfabccba26d069e97f784117ce265c0751e
emipa606 <emil.offermo@litet.net>
2021-07-20 22:56:02 +0200

    Updated for 1.3, dependency not updated but verified to work, added extra dependency for ovesized weapons.

--- Source/Gimmicks/ThoughtWorker_WearingWhip.cs
+++ Source/Gimmicks/ThoughtWorker_WearingWhip.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 
 namespace Gimmicks
 {
-	// Token: 0x02000003 RID: 3
-	public class ThoughtWorker_WearingWhip : ThoughtWorker
-	{
-		// Token: 0x06000002 RID: 2 RVA: 0x00002060 File Offset: 0x00000260
-		protected override ThoughtState CurrentStateInternal(Pawn p)
-		{
-			bool flag = p.equipment.Primary != null;
-			if (flag)
-			{
-				bool flag2 = p.equipment.Primary.def.defName == "Tool_Whip" || p.equipment.Primary.def.defName == "VWE_Tool_Whip";
-				bool flag3 = flag2;
-				if (flag3)
-				{
-					return ThoughtState.ActiveAtStage(0);
-				}
-			}
-			return ThoughtState.Inactive;
-		}
-	}
+    // Token: 0x02000003 RID: 3
+    public class ThoughtWorker_WearingWhip : ThoughtWorker
+    {
+        // Token: 0x06000002 RID: 2 RVA: 0x00002060 File Offset: 0x00000260
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (p.equipment.Primary == null)
+            {
+                return ThoughtState.Inactive;
+            }
+
+            if (p.equipment.Primary.def.defName == "Tool_Whip" ||
+                p.equipment.Primary.def.defName == "VWE_Tool_Whip")
+            {
+                return ThoughtState.ActiveAtStage(0);
+            }
+
+            return ThoughtState.Inactive;
+        }
+    }
 }
--- Source/Gimmicks/Trunken_LoadedGimmicks.cs
+++ Source/Gimmicks/Trunken_LoadedGimmicks.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using Verse;
+﻿using Verse;
 
 namespace Gimmicks
 {
-	// Token: 0x02000002 RID: 2
-	[StaticConstructorOnStartup]
-	public static class Trunken_LoadedGimmicks
-	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-		static Trunken_LoadedGimmicks()
-		{
-			Log.Message("Gimmicks ### Interstellar Encyclopedia Page 42 ### successfully loaded...", false);
-		}
-	}
+    // Token: 0x02000002 RID: 2
+    [StaticConstructorOnStartup]
+    public static class Trunken_LoadedGimmicks
+    {
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        static Trunken_LoadedGimmicks()
+        {
+            Log.Message("Gimmicks ### Interstellar Encyclopedia Page 42 ### successfully loaded...");
+        }
+    }
 }
